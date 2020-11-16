8333c439f698aa5283150bd618d7b05a40201198
Quangchien99 <phamquangchien170499@gmail.com>
2020-11-16 15:32:25 +0700

    add models: Pokemon and PokemonResponse

---
+++ app/src/main/java/com/example/pokemonapplication/model/Pokemon.java
@@ -0,0 +1,48 @@
+package com.example.pokemonapplication.model;
+
+public class Pokemon {
+    private int id;
+    private String name;
+    private String url;
+
+    public Pokemon() {
+    }
+
+    public Pokemon(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
